6ea0ccce5c9ebc0cee43017cf665b38b56bd2d0d
Albert Pham <pnphuong29@gmail.com>
2022-12-01 20:16:36 +0700

    Update content for package.json and README.md file

--- README.md
+++ README.md
@@ -1,24 +1,13 @@
-# Joplin Plugin
+# Remove notes without confirmation
 
-This is a template to create a new Joplin plugin.
+This plugin allows you to remove all of selected notes without confirmation.
 
-The main two files you will want to look at are:
+You can also choose to remove all associated attachments (images, etc.) of the selected notes when removing them.
 
-- `/src/index.ts`, which contains the entry point for the plugin source code.
-- `/src/manifest.json`, which is the plugin manifest. It contains information such as the plugin a name, version, etc.
+## Hot keys
 
-## Building the plugin
+- Remove all selected notes [CtrlOrCmd + Alt + Shift + D]
 
-The plugin is built using Webpack, which creates the compiled code in `/dist`. A JPL archive will also be created at the root, which can use to distribute the plugin.
+- Remove all selected notes and their associated attachments [CtrlOrCmd + Alt + D]
 
-To build the plugin, simply run `npm run dist`.
-
-The project is setup to use TypeScript, although you can change the configuration to use plain JavaScript.
-
-## Updating the plugin framework
-
-To update the plugin framework, run `npm run update`.
-
-In general this command tries to do the right thing - in particular it's going to merge the changes in package.json and .gitignore instead of overwriting. It will also leave "/src" as well as README.md untouched.
-
-The file that may cause problem is "webpack.config.js" because it's going to be overwritten. For that reason, if you want to change it, consider creating a separate JavaScript file and include it in webpack.config.js. That way, when you update, you only have to restore the line that include your file.
+- All hot keys are changeable in Preferences > Keyboard Shortcuts
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,75 @@
 import joplin from 'api';
+import { MenuItemLocation } from 'api/types';
 
 joplin.plugins.register({
-	onStart: async function() {
-		console.info('Hello world. Test plugin started!');
-	},
+    onStart: async function () {
+        await joplin.commands.register({
+            name: 'deleteNotesWithoutConfirmation',
+            label: 'Force delete notes',
+            execute: async (noteIds: string[]) => {
+                // If this command is triggered from menu <Tools> then noteIds will be null
+                // So we need to get all selected note ids in current workspace as below
+                if (!noteIds) {
+                    noteIds = await joplin.workspace.selectedNoteIds();
+                }
+
+                noteIds.forEach(async (noteId: string) => {
+                    await joplin.data.delete(['notes', noteId]);
+                });
+            },
+        });
+
+        await joplin.commands.register({
+            name: 'deleteNotesAndAssociatedAttachmentsWithoutConfirmation',
+            label: 'Force delete notes & all associated attachments',
+            execute: async (noteIds: string[]) => {
+                // If this command is triggered from menu <Tools> then noteIds will be null
+                // So we need to get all selected note ids in current workspace as below
+                if (!noteIds) {
+                    noteIds = await joplin.workspace.selectedNoteIds();
+                }
+
+                noteIds.forEach(async (noteId: string) => {
+                    const noteResources = await joplin.data.get([
+                        'notes',
+                        noteId,
+                        'resources',
+                    ]);
+                    noteResources.items.forEach(async (noteRes) => {
+                        await joplin.data.delete(['resources', noteRes.id]);
+                    });
+
+                    await joplin.data.delete(['notes', noteId]);
+                });
+            },
+        });
+
+        await joplin.views.menuItems.create(
+            'ToolsDeleteNotes',
+            'deleteNotesWithoutConfirmation',
+            MenuItemLocation.Tools,
+            { accelerator: 'CmdOrCtrl+Alt+Shift+D' }
+        );
+
+        await joplin.views.menuItems.create(
+            'noteListMenuItemDeleteNotes',
+            'deleteNotesWithoutConfirmation',
+            MenuItemLocation.NoteListContextMenu,
+            { accelerator: 'CmdOrCtrl+Alt+Shift+D' }
+        );
+
+        await joplin.views.menuItems.create(
+            'ToolsDeleteNotesAndAssociatedAttachments',
+            'deleteNotesAndAssociatedAttachmentsWithoutConfirmation',
+            MenuItemLocation.Tools,
+            { accelerator: 'CmdOrCtrl+Alt+D' }
+        );
+
+        await joplin.views.menuItems.create(
+            'noteListMenuItemDeleteNotesAndAssociatedAttachments',
+            'deleteNotesAndAssociatedAttachmentsWithoutConfirmation',
+            MenuItemLocation.NoteListContextMenu,
+            { accelerator: 'CmdOrCtrl+Alt+D' }
+        );
+    },
 });
